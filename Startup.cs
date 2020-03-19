ac795d7c10f4482a7df08ac5aa930d88c8361807
Luca Provenzano <lucaeprov@gmail.com>
2020-03-19 03:34:27 -0300

    First commit

---
+++ Startup.cs
@@ -0,0 +1,50 @@
+﻿using Hangfire;
+using Jobs.Tier;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Hangfire_SettingUp
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(x => x.UseSqlServerStorage(@"Server=localhost\SQLEXPRESS02;Database=hangfiretest;Trusted_Connection=True;"));
+            services.AddHangfireServer();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddScoped<ITestJob, TestJob>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IRecurringJobManager recurringJobManager, ITestJob job)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireDashboard();
+            app.UseMvc();
+
+            recurringJobManager.AddOrUpdate("created-by-hand", () => Console.WriteLine("created by hand!"), Cron.Minutely); //One job
+            job.EnqueueJobs(); //Bulk
+        }
+
+
+    }
+}
