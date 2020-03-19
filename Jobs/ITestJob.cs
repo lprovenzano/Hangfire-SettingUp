14e55b2612649cee49b4ac7b1ff9612f1db2f615
Luca Provenzano <lucaeprov@gmail.com>
2020-03-19 03:39:02 -0300

    TestJob

---
+++ Jobs/ITestJob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobs.Tier
+{
+    public interface ITestJob
+    {
+        Task Fire(int id);
+        void EnqueueJobs();
+    }
+}
---
+++ Jobs/TestJob.cs
@@ -0,0 +1,36 @@
+﻿using Hangfire;
+using System;
+using System.Threading.Tasks;
+
+namespace Jobs.Tier
+{
+    public class TestJob : ITestJob
+    {
+        public async Task Fire(int idJob)
+        {
+            Console.WriteLine("Fired!");
+            Console.WriteLine($"IDJob: {idJob}");
+
+            var message = await DoSomething();
+
+            Console.WriteLine(message);
+        }
+
+        public void EnqueueJobs()
+        {
+            RecurringJob.AddOrUpdate("jobNew1-id", () => Fire(11), Cron.Minutely);
+            RecurringJob.AddOrUpdate("jobNew2-id", () => Fire(22), Cron.Minutely);
+            RecurringJob.AddOrUpdate("jobNew3-id", () => Fire(33), Cron.Minutely);
+        }
+
+        private async Task<string> DoSomething()
+        {
+            int miliseconds = 200;
+            await Task.Delay(miliseconds);
+
+            return $"Done after {miliseconds} miliseconds.";
+
+        }
+
+    }
+}
